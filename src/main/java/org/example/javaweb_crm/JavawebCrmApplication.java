149c2f97773c1971d6d4ca87fe28016f892fdffa
cb <cb13551420764@163.com>
2025-11-01 10:49:25 +0800

    初始状态

---
+++ src/main/java/org/example/javaweb_crm/JavawebCrmApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaweb_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavawebCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaweb_crm/JavawebCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaweb_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawebCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
